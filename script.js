33939cabac717d7d1a170c1955dd3c638dd212cd
rooneyal <rooneyal@bc.edu>
2020-10-20 22:16:46 -0400

    com

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
-    console.log('coffee-house-chains.csv', data)
-
 const margin = {top:50, left:50, bottom:80, right:50};
 const width = 500-margin.left- margin.right;
 const height = 500-margin.top- margin.bottom;
@@ -12,44 +9,80 @@ const svg = d3.select('.chart')
     .append('g')
     .attr("transform", 'translate(' + margin.left + ',' + margin.top + ')');
 
-
 const xScale = d3.scaleBand()
-    .range([0, width])
-    .domain(data.map(function(d) {
-        return d.company;
-      }))
-    .padding(0.2);
-svg.append('g')
-      .attr("transform", 'translate(0,' + height + ')')
-      .call(d3.axisBottom(xScale))
-      .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
-
+    .rangeRound([0, width])
+    .paddingInner(0.1);
+    
+const xAxis = d3.axisBottom()
+      .scale(xScale);
 
 const yScale = d3.scaleLinear()
-    .domain([0, 22000])
     .range([height, 0]);
+
+const yAxis = d3.axisLeft()
+    .scale(yScale);
+
 svg.append('g')
-    .call(d3.axisLeft(yScale));
+    .attr('class', 'x-axis')
+    .call(xAxis)
+    .attr('transform', 'translate(' + 0 + ' , ' + height + ')');
+
+svg.append('g')
+    .attr('class', 'y-axis')
+    .call(yAxis)
+    .attr('transform', 'translate(' + 0 + ')');
 
 
-  svg.selectAll('chart')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('x', function(d) {
-        return xScale(d.company);
+d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{ 
+    type = document.querySelector("#group-by").value
+    let click = 0
+    update(data, type)
+    console.log('coffee-house-chains.csv', data)
+    document.querySelector('#sort')
+		addEventListener('click', (event) => {
+			if (click == false) {
+                click = true;
+            } else {
+                click = false;
+            }
+            update(data, type)
+        })
+    document.querySelector("#group-by").addEventListener('change', (event) => {
+        update(data, event.target.value)})
     })
-    .attr('y', function(d) {
-        return yScale(d.stores);
-    })
-    .attr('width', xScale.bandwidth())
-    .attr('height', function(d) {
-        return height - yScale(d.stores);
-    })
-    .attr('fill', 'lightblue')
 
 
+function update(data, type) {
+    type = document.querySelector("#group-by").value
 
-})
+    xScale.domain(data.map(d=>d.company))
+
+    yScale.domain([0, d3.max(data,d=>d[type])]);
+
+    const rects = svg.selectAll('rect')
+        .data(data, d=> {
+            return d.company;
+        });
+
+    rects.enter()
+        .append('rect')
+        .merge(rects)
+        .attr('x', d=>xScale(d.company))
+        .attr('y', d=>yScale(d[type]))
+        .attr('width', xScale.bandwidth())
+        .attr('height', function(d) {
+            return height - yScale(d[type]);
+        })
+        .attr('fill', 'brown')
+
+        rects.exit().remove();
+
+    svg.exit()
+        .remove();
+    }
+
+    svg.select('.x-axis')
+        .call(xAxis);
+        
+    svg.select('.y-axis')
+        .call(yAxis);
